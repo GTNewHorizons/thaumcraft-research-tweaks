0305a4d54e40d0d5ef79d3627ce6526fbb42020f
Telan <13066272+theelan@users.noreply.github.com>
2021-09-14 21:00:14 +0300

    configure mixins

---
+++ src/main/java/elan/Main.java
@@ -0,0 +1,23 @@
+package elan;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.event.*;
+import net.minecraft.init.*;
+
+@Mod(
+    modid = Main.MOD_ID, 
+    version = Main.VERSION, 
+    dependencies = "required-after:spongemixins@[1.1.0,);"
+)
+public class Main {
+
+    public static final String MOD_ID = "thaumcraft-research-tweaks";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+    }
+}
