8b1a48e9f47d839f916a686ffe8259c6daac69b6
Telan <13066272+theelan@users.noreply.github.com>
2021-09-15 20:29:35 +0300

    apply mixin in dev env; other misc changes

--- src/main/java/elan/thaumcraft/research/tweaks/Main.java
+++ src/main/java/elan/thaumcraft/research/tweaks/Main.java
@@ -1,19 +1,21 @@
 package elan.thaumcraft.research.tweaks;
 
+import static elan.thaumcraft.research.tweaks.Main.*;
+
 import cpw.mods.fml.common.*;
 import cpw.mods.fml.common.Mod.*;
 import cpw.mods.fml.common.event.*;
 import net.minecraft.init.*;
 
 @Mod(
-    modid = Main.MOD_ID, 
-    version = Main.VERSION, 
-    dependencies = "required-after:spongemixins@[1.1.0,);"
+    modid = MOD_ID, name = NAME, version = VERSION,
+    dependencies = "required-after:Thaumcraft;required-after:spongemixins@[1.1.0,);"
 )
 public class Main {
 
-    public static final String MOD_ID = "ThaumcraftResearchTweaks";
-    public static final String VERSION = "1.0";
+    public static final String MOD_ID = "thaumcraftresearchtweaks";
+    public static final String NAME = "Thaumcraft NEI Additions";
+    public static final String VERSION = "GRADLETOKEN_VERSION";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/elan/thaumcraft/research/tweaks/mixin/GuiResearchTableMixin.java
+++ src/main/java/elan/thaumcraft/research/tweaks/mixin/GuiResearchTableMixin.java
@@ -1,20 +1,14 @@
 package elan.thaumcraft.research.tweaks.mixin;
 
-import net.minecraft.client.gui.inventory.*;
-import net.minecraft.inventory.*;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.*;
 import thaumcraft.client.gui.*;
 
 @Mixin(GuiResearchTable.class)
-public abstract class GuiResearchTableMixin extends GuiContainer {
+public abstract class GuiResearchTableMixin {
 
-  public GuiResearchTableMixin(Container p_i1072_1_) {
-    super(p_i1072_1_);
-  }
-
-  @Inject(method = "drawAspects", at = @At("HEAD"), remap = false)
+  @Inject(method = "drawAspects", at = @At("HEAD"), remap = false, require = 1)
   private void onDrawAspects(int x, int y, CallbackInfo ci) {
     System.out.println("INJECTION WORKED x: " + x + ", y: " + y);
   }
