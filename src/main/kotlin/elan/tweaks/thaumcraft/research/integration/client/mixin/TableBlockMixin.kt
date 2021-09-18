569d35dcf20b66d303654b7fa60b92ab86fe4ff5
Telan <13066272+theelan@users.noreply.github.com>
2021-09-18 11:34:34 +0300

    use @Redirect instead of @ModifyArg to modify multiple arguments

--- src/main/kotlin/elan/tweaks/thaumcraft/research/integration/client/mixin/TableBlockMixin.kt
+++ src/main/kotlin/elan/tweaks/thaumcraft/research/integration/client/mixin/TableBlockMixin.kt
@@ -5,10 +5,7 @@ import elan.tweaks.thaumcraft.research.integration.client.gui.GuiHandler
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.world.World
 import org.spongepowered.asm.mixin.Mixin
-import org.spongepowered.asm.mixin.injection.At
-import org.spongepowered.asm.mixin.injection.Inject
-import org.spongepowered.asm.mixin.injection.ModifyArg
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import org.spongepowered.asm.mixin.injection.*
 import thaumcraft.common.blocks.BlockTable
 
 @Mixin(BlockTable::class)
@@ -18,51 +15,20 @@ abstract class TableBlockMixin {
         private const val THAUMCRAFT_RESEARCH_TABLE_GUI_ID = 10
     }
 
-    @ModifyArg(
+    @Redirect(
         method = ["onBlockActivated"],
         at = At(value = "INVOKE", target = "net.minecraft.entity.player.EntityPlayer.openGui(Ljava/lang/Object;ILnet/minecraft/world/World;III)V"),
-        index = 1,
         require = 4
     )
-    private fun correctModGuiIdFor(modGuiId: Int): Int {
-        println("correctModGuiIdFor($modGuiId)")
+    private fun correctGuiCallFor(entityPlayer: EntityPlayer, mod: Any, modGuiId: Int, world: World, x: Int, y: Int, z: Int) {
         return if (modGuiId == THAUMCRAFT_RESEARCH_TABLE_GUI_ID) {
-            GuiHandler.IDs.RESEARCH_TABLE
+            println("Calling new gui for guiId: $modGuiId")
+            entityPlayer.openGui(ThaumcraftResearchTweaks, GuiHandler.IDs.RESEARCH_TABLE, world, x, y, z)
         } else {
-            modGuiId
+            println("Calling original gui for guiId: $modGuiId")
+            entityPlayer.openGui(mod, modGuiId, world, x, y, z)
         }
     }
 
-    @ModifyArg(
-        method = ["onBlockActivated"],
-        at = At(value = "INVOKE", target = "net.minecraft.entity.player.EntityPlayer.openGui(Ljava/lang/Object;ILnet/minecraft/world/World;III)V"),
-        index = 0,
-        require = 4
-    )
-    private fun correctModFor(mod: Any, modGuiId: Int, world: World, x: Int, y: Int, z: Int): Any {
-        println("correctModFor($mod, $modGuiId)")
-        return if (modGuiId == THAUMCRAFT_RESEARCH_TABLE_GUI_ID) {
-            ThaumcraftResearchTweaks
-        } else {
-            mod
-        }
-    }
 
-    // I am not sure prevs are working...
-//    @Inject(
-//        method = ["onBlockActivated"],
-//        at = [At(value = "INVOKE", target = "net.minecraft.entity.player.EntityPlayer.openGui(Ljava/lang/Object;ILnet/minecraft/world/World;III)V")],
-//        require = 4,
-//        cancellable = true
-//    )
-//    private fun openCorrectGui(player: EntityPlayer, mod: Any, modGuiId: Int, world: World, x: Int, y: Int, z: Int, callback: CallbackInfo) {
-//        println("openCorrectGui($mod, $modGuiId)")
-//        if (modGuiId == THAUMCRAFT_RESEARCH_TABLE_GUI_ID) {
-//            println("Opening tweaked gui")
-//            player.openGui(ThaumcraftResearchTweaks, GuiHandler.IDs.RESEARCH_TABLE, world, x, y, z)
-//            callback.cancel()
-//        } else {
-//            println("Opening regular gui")
-//        }
-//    }
 }
