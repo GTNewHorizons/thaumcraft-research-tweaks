616ef4554c670a8058479c1a7c8aa24a1194b53d
Telan <13066272+TheElan@users.noreply.github.com>
2021-10-09 18:26:41 +0300

    bump forgelin to 1.9.2; remove debug condition opening old gui

--- src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/table/ThaumcraftResearchGuiHandler.kt
+++ src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/table/ThaumcraftResearchGuiHandler.kt
@@ -1,37 +1,38 @@
 package elan.tweaks.thaumcraft.research.frontend.integration.table
 
 import cpw.mods.fml.common.network.IGuiHandler
+import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks
 import elan.tweaks.thaumcraft.research.frontend.integration.table.ThaumcraftResearchGuiHandler.IDs.RESEARCH_TABLE
 import elan.tweaks.thaumcraft.research.frontend.integration.table.container.ResearchTableContainerFactory
 import elan.tweaks.thaumcraft.research.frontend.integration.table.gui.ResearchTableGuiFactory
-import net.minecraft.client.multiplayer.WorldClient
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.world.World
-import thaumcraft.client.gui.GuiResearchTable
-import thaumcraft.common.container.ContainerResearchTable
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 import thaumcraft.common.tiles.TileResearchTable
 
 class ThaumcraftResearchGuiHandler : IGuiHandler {
 
+    private val logger: Logger = LogManager.getLogger(ThaumcraftResearchTweaks.MOD_ID)
+
     object IDs {
         const val RESEARCH_TABLE = 0
     }
-    
+
     override fun getServerGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? =
-        if (id == RESEARCH_TABLE && x == -142 && y == 68 && z == 239)
-            ContainerResearchTable(player.inventory, world.getTileEntity(x, y, z) as TileResearchTable) // TODO: remove old GUI debug and coords in if
-        else if(id == RESEARCH_TABLE)
-            ResearchTableContainerFactory.create(player.inventory, world.getTableTileEntity(x, y, z))
-        else null
-    
+        if (id == RESEARCH_TABLE) ResearchTableContainerFactory.create(player.inventory, world.getTableTileEntity(x, y, z))
+        else unknownGui(id)
+
+
     override fun getClientGuiElement(id: Int, player: EntityPlayer, world: World, x: Int, y: Int, z: Int): Any? =
-        if (world is WorldClient && id == RESEARCH_TABLE && x == -142 && y == 68 && z == 239)
-            GuiResearchTable(player, world.getTileEntity(x, y, z) as TileResearchTable) // TODO: remove old GUI debug and coords in if
-        else if(id == RESEARCH_TABLE)
-            ResearchTableGuiFactory.create(player, world.getTableTileEntity(x, y, z))
-        else
-            null
-    
+        if (id == RESEARCH_TABLE) ResearchTableGuiFactory.create(player, world.getTableTileEntity(x, y, z))
+        else unknownGui(id)
+
+    private fun unknownGui(id: Int): Any? {
+        logger.error("Received unknown gui id: $id")
+        return null
+    }
+
     private fun World.getTableTileEntity(x: Int, y: Int, z: Int) =
         getTileEntity(x, y, z) as TileResearchTable
 
