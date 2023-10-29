f91946f6bf972dcb4c837f676a1aa54d6a56295a
Telan <13066272+TheElan@users.noreply.github.com>
2023-10-29 20:22:59 +0100

    make ThaumcraftResearchTweaks constant; enable spotless

--- src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/ThaumcraftResearchTweaks.kt
+++ src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/ThaumcraftResearchTweaks.kt
@@ -4,29 +4,20 @@ import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.SidedProxy
 import cpw.mods.fml.common.event.FMLLoadCompleteEvent
 import cpw.mods.fml.common.network.NetworkRegistry
-import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks.DEPENDENCIES
 import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks.MOD_ID
-import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks.MOD_LANGUAGE_ADAPTER
-import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks.NAME
-import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks.VERSION
 import elan.tweaks.thaumcraft.research.frontend.integration.proxies.SingletonInitializer
 import elan.tweaks.thaumcraft.research.frontend.integration.table.ThaumcraftResearchGuiHandler
 
 @Mod(
     modid = MOD_ID,
-    name = NAME,
-    version = VERSION,
-    dependencies = DEPENDENCIES,
-    modLanguageAdapter = MOD_LANGUAGE_ADAPTER
+    name = "Thaumcraft Research Tweaks",
+    version = MODVER,
+    modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter",
+    dependencies = "required-after:forgelin;required-after:spongemixins;required-after:Thaumcraft;"
 )
 object ThaumcraftResearchTweaks {
 
     const val MOD_ID = "ThaumcraftResearchTweaks"
-    const val VERSION = MODVER
-    const val NAME = "Thaumcraft Research Tweaks"
-    const val MOD_LANGUAGE_ADAPTER = "net.shadowfacts.forgelin.KotlinAdapter"
-    const val DEPENDENCIES =
-        "required-after:forgelin;" + "required-after:spongemixins;" + "required-after:Thaumcraft;"
 
     @SidedProxy(
         clientSide =
