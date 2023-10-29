2487f923a77f45a43168df723e548c160e677e43
Telan <13066272+TheElan@users.noreply.github.com>
2023-10-29 16:16:39 +0100

    initialise aspect tree on FMLLoadCompleteEvent event to reduce load time on first UI opening

--- src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/proxies/ClientSingletonInitializer.kt
+++ src/main/kotlin/elan/tweaks/thaumcraft/research/frontend/integration/proxies/ClientSingletonInitializer.kt
@@ -1,5 +1,6 @@
 package elan.tweaks.thaumcraft.research.frontend.integration.proxies
 
+import elan.tweaks.thaumcraft.research.frontend.domain.model.AspectTree
 import elan.tweaks.thaumcraft.research.frontend.integration.ThaumcraftResearchTweaks
 import elan.tweaks.thaumcraft.research.frontend.integration.table.gui.layout.ParchmentHexMapLayout
 import org.apache.logging.log4j.LogManager
@@ -8,6 +9,8 @@ import org.apache.logging.log4j.Logger
 class ClientSingletonInitializer : SingletonInitializer {
   private val logger: Logger = LogManager.getLogger(ThaumcraftResearchTweaks.MOD_ID)
   override fun initialize() {
+    val aspectTree = AspectTree
+    logger.debug("Initialised $aspectTree")
     val parchmentHexMap = ParchmentHexMapLayout
     logger.debug("Initialised $parchmentHexMap")
   }
